9c1f0c5003d48eaa66176d7bb701e369cca7c61e
TJ Dokas <tjdokas@gmail.com>
2021-06-27 21:19:19 -0400

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	deleted:    basic-input.txt 	modified:   input 	new file:   input_generator 	modified:   input_generator.cpp 	modified:   input_short.txt

--- input_generator.cpp
+++ input_generator.cpp
@@ -8,31 +8,83 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-    int n = atoi(argv[1]); // number of lines of input
-
-    vector<string> names = {"Tony","Jimmy","Perez","Michael","Chris","John","Sergio",
+const vector<string> names_list = {"Tony","Jimmy","Perez","Michael","Chris","John","Sergio",
                             "Philip","David","Paul","Mark","James","Andrew","Stuart",
                             "Gary","Kevin","Charles","Peter","Darren","George","Kenneth",
                             "Allan","Simon","Adam"};
-    string alpha = "abcdefghijklmnopqrstuvwxyz";
+const string alpha = "augc";
+int month = 1, day = 1, year = 2021;
 
-    for (size_t i = 0; i < n; i++)
+// lets say that mutations take place on charicteristic timescales of a week
+void date_helper() {
+    int infectDay = day + rand() % 7;
+    day += infectDay;
+    if (day > 30)
     {
-        // dates
-        cout << rand() % 12 + 1 << "/" << rand() % 28 + 1 << "/";
-        cout << rand() % 2 + 2020 << " ";
-
-        // names
-        cout << names[rand() % names.size()] << " ";
-        cout << names[rand() % names.size()] << " ";
-
-        // disease string sequence
-        for (size_t j = 0; j < 32; j++)
+        day = day % 30;
+        month++;
+        if (month > 12)
         {
-            cout << alpha[rand() % alpha.size()];
+            month = month % 12;
+            year++;
+            cout << month << "/" << day << "/" << year << " ";
+            month += 11;
+            year--;
         }
-        cout << "\n";
+        else
+        {
+            cout << month << "/" << day << "/" << year << " ";
+            day += (30 - infectDay);
+            month--;
+        }
     }
+    else
+    {
+        cout << month << "/" << day << "/" << year << " ";
+        day -= infectDay;
+    }
+}
+
+void disease_helper(string genome,vector<pair<string,string>> names, int &n) {
+    if (n <= 0)
+    {
+        return;
+    }
+    
+    vector<pair<string,string>> newlyInfected;
+    for (size_t i = 0; i < names.size(); i++)
+    {
+        date_helper();
+        cout << names[i].first << " " << names[i].second << " ";
+        cout << genome << "\n";
+        string mutant = genome;
+        mutant[rand() % mutant.size()] = alpha[rand() % 4];
+        day += 7;
+        for (size_t newPatient = 0; newPatient < rand() % 3; newPatient++)
+        {
+            newlyInfected.push_back({ names_list[rand() % names_list.size()] , names_list[rand() % names_list.size()] });
+        }
+        disease_helper(mutant,newlyInfected, n);
+    }
+    n--;
+}
+
+int main(int argc, char** argv) {
+    int n = atoi(argv[1]); // generations of mutations
+    int people = 4; // TODO: make number of people variable
+    vector<pair<string,string>> names(people); 
+    string genomeNought;
+
+    for (size_t i = 0; i < people; i++)
+    {
+        names.push_back({ names_list[rand() % names_list.size()] , names_list[rand() % names_list.size()] });
+    }
+    
+    for (size_t i = 0; i < 32; i++)
+    {
+        genomeNought.push_back(alpha[rand() % 4]);
+    }
+    
+    disease_helper(genomeNought,names,n);
     return 0;
 }
